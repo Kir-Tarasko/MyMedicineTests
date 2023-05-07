932c66124e34beebce78f310933cb7c61b67e390
KirTarasko <tarasko.kirill@icloud.com>
2023-05-07 10:30:36 +0200

    Initial Commit

---
+++ GPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPT
+//
+//  Created by Kirill Tarasko on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
