56d62a51b49e29c12341f736eeb4939e04927a8e
KirTarasko <tarasko.kirill@icloud.com>
2023-05-11 19:16:53 +0200

    Subwiews optimization

---
+++ GPT/SignInVC.swift
@@ -0,0 +1,108 @@
+//
+//  SignInVC.swift
+//  GPT
+//
+//  Created by Kirill Tarasko on 07.05.2023.
+//
+
+import UIKit
+
+class SignInVC: UIViewController, UITextFieldDelegate {
+   
+    lazy var loginView = LoginView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        view.addSubview(loginView)
+        setLoginButton(enabled: false)
+        textFieldSetup()
+    }
+    
+}
+
+extension SignInVC {
+    
+    private func textFieldSetup() {
+        
+        let textFields = [loginView.loginTextField, loginView.passwordTextField ]
+        
+        for textField in textFields {
+            textField.delegate = self
+            textField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        }
+        
+        textFields.first?.tag = 0
+        textFields.first?.returnKeyType = .next
+        
+        textFields.last?.tag = 1
+        textFields.last?.returnKeyType = .done
+        
+        registerKeyboardNotification()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    @objc private func textFieldChanged() {
+        
+        guard
+            let login = loginView.loginTextField.text,
+            let password = loginView.passwordTextField.text
+        else { return }
+        
+        let formFilled = !(login.isEmpty) && !(password.isEmpty)
+        
+        setLoginButton(enabled: formFilled)
+    }
+    
+    private func setLoginButton(enabled:Bool) {
+
+        if enabled {
+            loginView.loginButton.alpha = 1.0
+            loginView.loginButton.isEnabled = true
+        } else {
+            loginView.loginButton.alpha = 0.9
+            loginView.loginButton.isEnabled = false
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField, loginView.loginTextField.text != "" {
+            nextField.becomeFirstResponder()
+        } else if textField == self.loginView.passwordTextField {
+            textField.resignFirstResponder()
+            
+        }
+        return true
+    }
+    
+    func registerKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardNotification() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SignInVC.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        self.view.frame.origin.y = -250
+        
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        self.view.frame.origin.y = 0
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
+
+
--- GPT/ViewController.swift
+++ GPT/ViewController.swift
@@ -7,13 +7,125 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginView: UIView {
+    
+    // Создание полей ввода логина и пароля
+    let loginTextField: UITextField = {
+        let textField = UITextField()
+        let placeholderText = NSAttributedString(string: "Логин", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
+        textField.attributedPlaceholder = placeholderText
+        textField.textColor = .black
+        textField.layer.cornerRadius = 15.0
+        textField.borderStyle = .roundedRect
+        textField.layer.borderWidth = 2.0
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.autocapitalizationType = .none
+        textField.backgroundColor = .white
+        textField.textAlignment = .center
+        textField.clearsOnBeginEditing = false
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Пароль"
+        textField.textColor = .black
+        textField.layer.cornerRadius = 15.0
+        textField.borderStyle = .roundedRect
+        textField.layer.borderWidth = 2.0
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .white
+        textField.textAlignment = .center
+        textField.clearsOnBeginEditing = false
+        return textField
+    }()
+    
+    // Создание кнопки регистрации
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Регистрация", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Вход", for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 15
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // Инициализация View и добавление на нее элементов
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let subwiews = [loginTextField, passwordTextField, registerButton, loginButton]
+        
+        for subwiew in subwiews {
+            addSubview(subwiew)
+            subwiew.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+//        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+//        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+//        registerButton.translatesAutoresizingMaskIntoConstraints = false
+//        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // Размещение поля ввода логина
+            loginTextField.centerXAnchor.constraint(equalTo: centerXAnchor),
+            loginTextField.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -50),
+            loginTextField.widthAnchor.constraint(equalToConstant: 200),
+            loginTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            // Размещение поля ввода пароля
+            passwordTextField.centerXAnchor.constraint(equalTo: centerXAnchor),
+            passwordTextField.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 10),
+            passwordTextField.widthAnchor.constraint(equalToConstant: 200),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            loginButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            loginButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100),
+            loginButton.widthAnchor.constraint(equalToConstant: 150),
+            loginButton.heightAnchor.constraint(equalToConstant: 40),
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Размещение кнопки регистрации
+            registerButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            registerButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            registerButton.widthAnchor.constraint(equalToConstant: 150),
+            registerButton.heightAnchor.constraint(equalToConstant: 40),
+        ])
     }
-
-
+    
+    // Проверка логина и пароля при нажатии на кнопку регистрации
+    @objc func registerButtonTapped() {
+        guard let login = loginTextField.text, let password = passwordTextField.text else {
+            return
+        }
+        
+        if login == "Kir" && password == "123456" {
+            print("Вход выполнен")
+        } else {
+            print("Неверный логин или пароль")
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.placeholder = nil
+        passwordTextField.placeholder = nil
+    }
 }
+
+
+
+
 
